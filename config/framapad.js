1f73b3cfab125ced5ef4ade87a0b1c8a12478563
JosephK <josephk@framasoft.org>
2013-11-15 17:45:14 +0100

    Update framapad.js

--- config/framapad.js
+++ config/framapad.js
@@ -1,5 +1,7 @@
 var f$_config = 'local';
 
+var f$_jquery = 'fQuery'
+
 var f$_modal_don_txtdl1 = 'd\'utiliser';
 var f$_modal_don_txtdl2 = 'créer un pad';
 var f$_modal_don_liendl = 'a[href*=".framapad.org/p/"]';
