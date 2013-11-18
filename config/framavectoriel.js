ecca53fd54038ae8993651c44cbe2e28d504fc83
JosephK <josephk@framasoft.org>
2013-11-18 09:56:43 +0100

    Update framavectoriel.js

--- config/framavectoriel.js
+++ config/framavectoriel.js
@@ -1,8 +1,9 @@
 var f$_config = 'local';
 
-var f$_jquery = 'fQuery';
-
 var f$_css_position = 'start';
+
+var f$_alert_type = 'info'; 
+var f$_alert_text = '<p><strong>Attention</strong> Le logiciel ne fonctionne malheureusement plus correctement sur Firefox à partir de la version 23.</p>';
 
 var f$_modal_don_txtdl1 = 'd\'utiliser';
 var f$_modal_don_txtdl2 = 'créer une image';
