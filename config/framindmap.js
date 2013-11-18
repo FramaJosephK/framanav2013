de604c203ab8af534cda2aced876472870ddde8e
ego <ego@k.home>
2013-11-18 10:52:24 +0100

    framindmap.js renommé

---
+++ config/framindmap.js
@@ -0,0 +1,9 @@
+var f$_config = 'local';
+
+var f$_jquery = 'fQuery';
+
+var f$_css_position = 'start';
+
+var f$_modal_don_txtdl1 = 'd\'utiliser';
+var f$_modal_don_txtdl2 = 'créer une carte mentale';
+var f$_modal_don_liendl = 'a.btn-large[href*="/framindmap.html"]';
