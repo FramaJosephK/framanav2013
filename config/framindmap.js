680464e6fe6448d05ea51ce025ffcea88c2546b8
JosephK <josephk@framasoft.org>
2013-11-18 14:43:40 +0100

    Update framindmap.js

--- config/framindmap.js
+++ config/framindmap.js
@@ -2,6 +2,10 @@ var f$_config = 'local';
 
 var f$_jquery = 'fQuery';
 
+var f$_nav_extra_css = true;
+
 var f$_modal_don_txtdl1 = 'd\'utiliser';
 var f$_modal_don_txtdl2 = 'créer une carte mentale';
 var f$_modal_don_liendl = 'a.btn-large[href*="/framindmap.html"]';
+
+var f$_donate = false;
