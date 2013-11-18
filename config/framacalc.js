0e1947e088324cc3af989394ffa5e756aff2d756
JosephK <josephk@framasoft.org>
2013-11-18 12:28:27 +0100

    Update framacalc.js

--- config/framacalc.js
+++ config/framacalc.js
@@ -1,9 +1,5 @@
 var f$_config = 'local';
 
-var f$_jquery = 'fQuery';
-
-var f$_css_position = 'start';
-
 var f$_modal_don_txtdl1 = 'd\'utiliser';
 var f$_modal_don_txtdl2 = 'créer un calc';
 var f$_modal_don_liendl = 'a.btn-large[href*="/_new"]';
