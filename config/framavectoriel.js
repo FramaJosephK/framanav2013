14ee3dd7bf3ab90b51267f62393658ba80b058fc
JosephK <josephk@framasoft.org>
2013-11-18 16:15:07 +0100

    Update framavectoriel.js

--- config/framavectoriel.js
+++ config/framavectoriel.js
@@ -2,8 +2,14 @@ var f$_config = 'local';
 
 var f$_jquery = 'fQuery';
 
+if(window.location.indexOf('svg-editor') > -1) {
+// Si on n'est dans svg-editor
+var f$_extra_css = true;
+} else {
+// Si on est sur la page d'accueil
 var f$_alert_type = 'info'; 
 var f$_alert_text = '<p style="text-align:center"><strong>Attention</strong> Le logiciel ne fonctionne malheureusement plus correctement sur Firefox à partir de la version 23.</p>';
+}
 
 var f$_modal_don_txtdl1 = 'd\'utiliser';
 var f$_modal_don_txtdl2 = 'créer une image';
