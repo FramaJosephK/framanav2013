25bb47198a0387de5291c773178ee1af4d1d47df
JosephK <josephk@framasoft.org>
2013-11-19 16:34:05 +0100

    Update lite.framapad.js

--- config/lite.framapad.js
+++ config/lite.framapad.js
@@ -2,6 +2,10 @@ var f$_config = 'local';
 
 var f$_jquery = 'fQuery';
 
-var f$_donate = false;
-
 var f$_nav_extra_css = true;
+
+// Force la désactivation des fenêtres modales, du bandeau et du macaron
+var f$_alert_text = '';
+var f$_alert_modal_text = '';
+var f$_modal_don_liendl = '';
+var f$_donate = false;
