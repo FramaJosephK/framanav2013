58cd65f58f93b391de239219640071f9fc7579b5
JosephK <josephk@framasoft.org>
2013-11-19 16:29:48 +0100

    Update framavectoriel.js

--- config/framavectoriel.js
+++ config/framavectoriel.js
@@ -5,14 +5,21 @@ var f$_jquery = 'fQuery';
 var f$_url = window.location.href;
 if(f$_url.indexOf('svg-editor') > -1) {
 // Si on n'est dans svg-editor
-var f$_nav_extra_css = true;
-var f$_donate = false;
-} else {
-// Si on est sur la page d'accueil
-var f$_alert_type = 'info'; 
-var f$_alert_text = '<p style="text-align:center"><strong>Information</strong> : Framavectoriel a été mis à jour (<a href="https://code.google.com/p/svg-edit/">version 2.6 de SVG-Edit</a>) et fonctionne de nouveau avec les versions récentes de Firefox.</p>';
-}
+  var f$_nav_extra_css = true;
 
-var f$_modal_don_txtdl1 = 'd\'utiliser';
-var f$_modal_don_txtdl2 = 'créer une image';
-var f$_modal_don_liendl = 'a.btn-large[href*="/svg-editor.html"]';
+// Force la désactivation des fenêtres modales, du bandeau et du macaron
+  var f$_alert_text = '';
+  var f$_alert_modal_text = '';
+  var f$_modal_don_liendl = '';
+  var f$_donate = false;
+
+} else {
+
+// Si on est sur la page d'accueil
+  var f$_alert_type = 'info'; 
+  var f$_alert_text = '<p style="text-align:center"><strong>Information</strong> : Framavectoriel a été mis à jour (<a href="https://code.google.com/p/svg-edit/">version 2.6 de SVG-Edit</a>) et fonctionne de nouveau avec les versions récentes de Firefox.</p>';
+
+  var f$_modal_don_txtdl1 = 'd\'utiliser';
+  var f$_modal_don_txtdl2 = 'créer une image';
+  var f$_modal_don_liendl = 'a.btn-large[href*="/svg-editor.html"]';
+}
