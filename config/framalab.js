5dd23e970d7ec45e06128ade62364144c88d6753
JosephK <josephk@framasoft.org>
2013-11-20 10:00:58 +0100

    Update framalab.js

--- config/framalab.js
+++ config/framalab.js
@@ -1,7 +1,7 @@
 var f$_config = 'local';
 
 var f$_url = window.location.href;
-if(f$_url.indexOf('accueil') > -1) {
+if(f$_url.indexOf('/accueil') > -1) {
 // Sur la page d'accueil de test
 	// Alertes
 	var f$_alert_type = 'don';
@@ -23,4 +23,12 @@ if(f$_url.indexOf('accueil') > -1) {
 					'<p style="font-weight:bold; text-align:center">Merci de votre soutien <a href="http://soutenir.framasoft.org/?f=bandeau2">soutenir.framasoft.org</a></p></div>';
 	}
 
+}
+
+if(f$_url.indexOf('/bot') > -1) {
+	// Force la désactivation des fenêtres modales, du bandeau et du macaron
+	var f$_alert_text = '';
+  	var f$_alert_modal_text = '';
+  	var f$_modal_don_liendl = '';
+  	var f$_donate = false;
 }
