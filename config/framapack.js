59c15e0a1d03bbbb21e1388f8036267880bb0e16
JosephK <josephk@framasoft.org>
2013-11-21 16:52:53 +0100

    Update framapack.js

--- config/framapack.js
+++ config/framapack.js
@@ -2,6 +2,8 @@ var f$_config = 'local';
 
 var f$_jquery = 'fQuery';
 
+var f$_nav_extra_css = 'true';
+
 var f$_alert_type = 'danger';
 var f$_alert_text = '<p style="text-align:center; font-weight:bold">Framapack est en panne depuis la migration du serveur fin juin 2013.<br />Nous ne savons pas quand nous pourrons résoudre le problème. Désolé... :-(</p>';
 
