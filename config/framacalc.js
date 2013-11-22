6f7819035031eda85476f8ad055964f821a38b93
JosephK <josephk@framasoft.org>
2013-11-22 15:13:45 +0100

    Update framacalc.js

--- config/framacalc.js
+++ config/framacalc.js
@@ -1,5 +1,26 @@
 var f$_config = 'local';
 
-var f$_modal_don_txtdl1 = 'd\'utiliser';
-var f$_modal_don_txtdl2 = 'créer un calc';
-var f$_modal_don_liendl = 'a.btn-large[href*="/_new"]';
+var f$_url = window.location.href;
+if(f$_url.indexOf('framacalc.org/_start') > -1) {
+// Si on est sur la page d'accueil
+ var f$_modal_don_txtdl1 = 'd\'utiliser';
+ var f$_modal_don_txtdl2 = 'créer un calc';
+ var f$_modal_don_liendl = 'a.btn-large[href*="/_new"]';
+ 
+} else {
+// Si on n'est dans ethercalc
+  if (f$_not_in_frame) {
+    var f$_nav_extra_css = true;
+  } else {
+    var f$_bootstrap_css = false;
+  }
+
+// Force la désactivation des fenêtres modales, du bandeau et du macaron
+  var f$_alert_text = '';
+  var f$_alert_modal_text = '';
+  var f$_modal_don_liendl = '';
+  var f$_donate = false;
+}
+
+
+
