afc13228ac0ad34ef095ef20dc8237f3006581ca
JosephK <josephk@framasoft.org>
2013-11-28 11:18:54 +0100

    Update framalab.js

--- config/framalab.js
+++ config/framalab.js
@@ -4,21 +4,6 @@ var f$_url = window.location.href;
 if(f$_url.indexOf('/accueil') > -1) {
 // Sur la page d'accueil de test
 
-var f$_bootstrap_css = false;
-
-var f$_bootstrap_link = document.createElement('link');
-    f$_bootstrap_link.rel = "stylesheet";
-    f$_bootstrap_link.media="screen";
-    f$_bootstrap_link.href= f$_nav+"lib/bootstrap/css/bootstrap.min.css";    
-
-switch (f$_css_position) {
-	case 'start' : document.getElementsByTagName('head')[0].insertBefore(f$_bootstrap_link, document.getElementsByTagName('head')[0].firstChild);break;
-	case 'next' : document.getElementById('nav_js').parentNode.insertBefore(f$_bootstrap_link, document.getElementById('nav_js').nextSibling);break;
-	case 'end' : document.getElementsByTagName('head')[0].appendChild(f$_bootstrap_link);break;
-}				
-console.log('Test bootstrap.min.css');
-
-
 }
 
 if(f$_url.indexOf('/bot') > -1) {
