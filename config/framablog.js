795e22e04725772921939ad3cf84d30ad7d82c7c
JosephK <josephk@framasoft.org>
2013-12-12 10:21:07 +0100

    Update framablog.js

--- config/framablog.js
+++ config/framablog.js
@@ -3,3 +3,12 @@ var f$_config = 'local';
 var f$_jquery = 'fQuery';
 
 var f$_video_js = true;
+
+
+// Sous-titres dans les vidéos (jQuery 1.4.2 est déjà présent sur le framablog)
+jQuery(document).ready(function() {
+  jQuery('video').each(function(){
+    id = jQuery(this).attr('id').replace('k-','');
+    jQuery(this).children('track').attr('src', 'http://www.framablog.org/public/playk/cache/'+id+'/fr.vtt');
+  });
+});
