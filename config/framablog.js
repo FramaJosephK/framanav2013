b06ed1e476f88c4059179ef1d97608c4fe5fd060
JosephK <josephk@framasoft.org>
2014-01-06 16:23:54 +0100

    Update framablog.js

--- config/framablog.js
+++ config/framablog.js
@@ -2,8 +2,8 @@ var f$_config = 'local';
 
 var f$_jquery = 'fQuery';
 
+var f$_audio_js = true;
 var f$_video_js = true;
-
 
 // Sous-titres dans les vidéos (jQuery 1.4.2 est déjà présent sur le framablog)
 jQuery(document).ready(function() {
@@ -19,10 +19,4 @@ var f$_url = window.location.href;
 if(f$_url.indexOf('no-es-una-crisis-documentaire-libre') > -1) {
 // Si on est sur no-es-una-crisis
   var f$_nav_extra_js = true;
-}
-
-// Audio JS
-var f$_url = window.location.href;
-if(f$_url.indexOf('preview/josephk') > -1) {
-  var f$_audio_js = true;
 }
