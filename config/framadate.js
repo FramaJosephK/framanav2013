dbbbaab42c47f17837ff8574b4c23626994f0c0a
JosephK <josephk@framasoft.org>
2014-01-07 16:12:23 +0100

    Opt-in

--- config/framadate.js
+++ config/framadate.js
@@ -3,3 +3,10 @@ var f$_config = 'local';
 var f$_modal_don_txtdl1 = 'd\'utiliser';
 var f$_modal_don_txtdl2 = 'créer un sondage';
 var f$_modal_don_liendl = 'a[href*="./infos_sondage.php?choix_sondage="]';
+
+// Tests
+// Opt-in
+var f$_url = window.location.href;
+if(f$_url.indexOf('infos_sondage.php?fs=test&choix_sondage') > -1) {
+  f$_email_field1 = '#formulaire input[name="adresse"]';
+}
