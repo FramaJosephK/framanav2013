345777d343ffa51fc09e993f05505137687e2936
JosephK <josephk@framasoft.org>
2014-01-07 15:36:31 +0100

    Update forum.framasoft.js

--- config/forum.framasoft.js
+++ config/forum.framasoft.js
@@ -1,6 +1,7 @@
 var f$_config = 'local';
 
 var f$_css_position = 'end';
-
-f$_email_field1 = '#email_confirm';
-f$_email_field2 = '#email';
+/*
+var f$_email_field1 = '#email_confirm';
+var f$_email_field2 = '#email';
+*/
