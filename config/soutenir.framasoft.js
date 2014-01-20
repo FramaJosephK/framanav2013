89cebc22ae7776d7c9f221c1ae563f4869ae25f7
JosephK <josephk@framasoft.org>
2014-01-20 16:58:49 +0100

    Update soutenir.framasoft.js

--- config/soutenir.framasoft.js
+++ config/soutenir.framasoft.js
@@ -12,10 +12,8 @@ var f$_alert_modal_text = '';
 var f$_modal_don_liendl = '';
 var f$_donate = false;
 
-
-// Tests
 // Opt-in
 var f$_url = window.location.href;
-if(f$_url.indexOf('add/temoignage&fs=test') > -1) {
+if(f$_url.indexOf('add/temoignage') > -1) {
   f$_email_field1 = '#edit-field-t-email-0-value';
 }
