f07aa3f10468146eef5e9b5dd865b3f07a5f8168
JosephK <josephk@framasoft.org>
2014-01-20 16:58:14 +0100

    Update framasoft.js

--- config/framasoft.js
+++ config/framasoft.js
@@ -1,7 +1,4 @@
 var f$_config = 'local';
 
 // Opt-in
-var f$_url = window.location.href;
-if(f$_url.indexOf('spip.php?page=forum') > -1) {
-  f$_email_field1 = '#email_auteur';
-}
+var f$_email_field1 = '#email_auteur';
