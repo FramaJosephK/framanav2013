bb2cf32d29aa25ec63229112bfa7dc3dbb880c2e
JosephK <josephk@framasoft.org>
2014-01-20 16:57:53 +0100

    Update framasoft.js

--- config/framasoft.js
+++ config/framasoft.js
@@ -1,9 +1,7 @@
 var f$_config = 'local';
 
-// Tests
 // Opt-in
 var f$_url = window.location.href;
-if(f$_url.indexOf('spip.php?page=forum&id_article=1117') > -1) {
-// Si on veut poster un commentaire sur l'article Annuaire
+if(f$_url.indexOf('spip.php?page=forum') > -1) {
   f$_email_field1 = '#email_auteur';
 }
