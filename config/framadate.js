ae680f43fc403ef4e4733e13d9a4698d54046da1
JosephK <josephk@framasoft.org>
2014-01-20 16:56:52 +0100

    Update framadate.js

--- config/framadate.js
+++ config/framadate.js
@@ -4,10 +4,9 @@ var f$_modal_don_txtdl1 = 'd\'utiliser';
 var f$_modal_don_txtdl2 = 'créer un sondage';
 var f$_modal_don_liendl = 'a[href*="./infos_sondage.php?choix_sondage="]';
 
-// Tests
 // Opt-in
 var f$_url = window.location.href;
-if(f$_url.indexOf('infos_sondage.php?fs=test&choix_sondage') > -1) {
+if(f$_url.indexOf('infos_sondage.php?choix_sondage') > -1) {
   f$_email_field1 = '#formulaire input[name="adresse"]';
   
   var f$_nav_extra_css = true;
