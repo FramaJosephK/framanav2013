1fa1fb8612becc0266e6d267af08c67b0858ebb7
JosephK <josephk@framasoft.org>
2014-01-22 09:49:15 +0100

    Update wiki2.framasoft.js

--- config/wiki2.framasoft.js
+++ config/wiki2.framasoft.js
@@ -9,7 +9,7 @@ if(f$_url.indexOf('/asso/') > -1) {
       f$_extra_css_link.rel = "stylesheet";
       f$_extra_css_link.media="screen";
       f$_extra_css_link.href= f$_nav+"config/wiki2.framasoft_extra.css";
-  document.getElementById('nav_js').parentNode.insertBefore(f$_extra_css_link, document.getElementById('nav_js').nextSibling);break;
+  document.getElementById('nav_js').parentNode.insertBefore(f$_extra_css_link, document.getElementById('nav_js').nextSibling);
 
 } else {
   var f$_nav_extra_css = true;
