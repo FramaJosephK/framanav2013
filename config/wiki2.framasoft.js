03f66152f1c39be22ebc5efd9c784536a70d9f2b
JosephK <josephk@framasoft.org>
2014-01-22 09:47:03 +0100

    Test extra CSS sans attendre le DOM

--- config/wiki2.framasoft.js
+++ config/wiki2.framasoft.js
@@ -1,5 +1,17 @@
 var f$_config = 'local';
 
-var f$_nav_extra_css = true;
-
 var f$_alert_text = '';
+
+var f$_url = window.location.href;
+if(f$_url.indexOf('/asso/') > -1) {
+  // Sur la page d'accueil de test
+  var f$_extra_css_link = document.createElement('link');
+      f$_extra_css_link.rel = "stylesheet";
+      f$_extra_css_link.media="screen";
+      f$_extra_css_link.href= f$_nav+"config/wiki2.framasoft_extra.css";
+  document.getElementById('nav_js').parentNode.insertBefore(f$_extra_css_link, document.getElementById('nav_js').nextSibling);break;
+
+} else {
+  var f$_nav_extra_css = true;
+}
+            
