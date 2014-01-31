a9085e41c3944c9454a7ecdb9e288896fc6178fa
JosephK <josephk@framasoft.org>
2014-01-31 15:47:21 +0100

    Message arrêt de la création des pads

--- config/lite.framapad.js
+++ config/lite.framapad.js
@@ -6,6 +6,14 @@ var f$_nav_extra_css = true;
 
 // Force la désactivation des fenêtres modales, du bandeau et du macaron
 var f$_alert_text = '';
-var f$_alert_modal_text = '';
 var f$_modal_don_liendl = '';
 var f$_donate = false;
+
+
+// Message arrêt de la création des pads
+var f$_alert_modal_title = 'Création des pads désactivée';
+var f$_alert_modal_text = '<p>Nous vous informons que cette instance de Framapad (<b>lite</b>.framapad.org) ne peut plus accueillir de nouveaux pads.<p>'+
+    '<p>Il reste bien évidement possible de travailler sur les pads déjà éxistant (ils ne seront pas supprimés)'+
+    'mais pour créer de nouveaux pads, veuillez passer par la page d\'accueil du site :<br />'+
+    '<a href="http://www.framapad.org">www.framapad.org</a></p>'+
+    'Merci.<br />L\'équipe technique';
