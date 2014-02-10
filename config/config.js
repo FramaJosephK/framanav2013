496ff3779cf035b716c6020c7a2b8499d7e37471
JosephK <josephk@framasoft.org>
2014-02-10 18:58:04 +0100

    Update config.js

--- config/config.js
+++ config/config.js
@@ -29,11 +29,11 @@ var f$_alert_type = 'black'; 					// warning = jaune ; danger = rouge ; info = b
 var f$_alert_cookie = 7*24*60*60*1000;				// durée d'expiration du cookie en milliseconde
 var f$_alert_text =						// /!\ aux \' dans le texte
 '<div style="margin:0 auto; max-width:800px;font-weight:bold;">'+
-'<p style="font-size:13px">Framasoft s\'associe à la protestation générale des internautes contre '+
+'<p style="font-size:13px">Framasoft s’associe au mouvement général contre '+
 'la surveillance massive de nos données révélée par Edward Snowden.<br /> Le '+
-'11 février est "la journée de la contre attaque" commémorant les '+
+'11 février est « la journée de la contre attaque » commémorant les '+
 'victoires obtenues contre les lois ACTA, SOPA et PIPA et en souvenir '+
-'d\'Aaron Schwartz.</p>'+
+'d’Aaron Schwartz.</p>'+
 '<p style="text-align:center">'+
 '<a href="http://www.framablog.org/index.php/post/2014/02/10/the-day-we-fight-back" style="color:#f2c45a;border:none">Participez à la protestation</a>'+
 '</p>'+
