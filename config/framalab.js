94b608c6a61fca08399c1f7c541714acd9af2366
JosephK <josephk@framasoft.org>
2014-02-17 11:50:15 +0100

    Update framalab.js

--- config/framalab.js
+++ config/framalab.js
@@ -15,18 +15,13 @@ if(f$_url.indexOf('/bot') > -1) {
 }
 if(f$_url.indexOf('/accueil') > -1) {
 	// Force la désactivation des fenêtres modales, du bandeau et du macaron
-	var f$_alert_type = 'black';
+	var f$_alert_type = 'info';
 	var f$_alert_text = 
-		'<div style="margin:0 auto; max-width:800px;font-weight:bold;">'+
-			'<p style="font-size:13px">Framasoft s\'associe à la protestation générale des internautes contre '+
-			'la surveillance massive de nos données révélée par Edward Snowden.<br /> Le '+
-			'11 février est "la journée de la contre attaque" commémorant les '+
-			'victoires obtenues contre les lois ACTA, SOPA et PIPA et en souvenir '+
-			'd\'Aaron Schwartz.</p>'+
-			'<p style="text-align:center">'+
-				'<a href="http://www.framablog.org/index.php/post/2014/02/10/the-day-we-fight-back" style="color:#f2c45a;border:none">Participez à la protestation</a>'+
-			'</p>'+
-		'</div>';
+		'<div style="text-align:center"><a href="http://april.org/campagne">'+
+		'<img width="580" height="76"'+
+			'src="http://april.org/campagne/images/priorite-logiciel-libre-je-soutiens-april.png"'+
+			'alt="Priorité au Logiciel Libre! Je soutiens l’April." />'+
+		'</a></div>';
   	var f$_alert_modal_text = '';
   	var f$_modal_don_liendl = '';
  // 	var f$_donate = false;
