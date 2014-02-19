a28a5c338bf4ac4f59657ffe8fe192263a6cc26b
JosephK <josephk@framasoft.org>
2014-02-19 11:38:00 +0100

    Ajout piwik

--- config/framablog.js
+++ config/framablog.js
@@ -13,5 +13,19 @@ jQuery(document).ready(function() {
   });
 });
 
-
+// Opt-in
 var f$_email_field1 = '#c_mail';
+
+/** Piwik **/
+var _paq = _paq || [];
+_paq.push(["trackPageView"]);
+_paq.push(["enableLinkTracking"]);
+
+(function() {
+  var u=(("https:" == document.location.protocol) ? "https" : "http") + "://stats.framasoft.org/";
+  _paq.push(["setTrackerUrl", u+"piwik.php"]);
+  _paq.push(["setSiteId", "3"]);
+  var d=document, g=d.createElement("script"), s=d.getElementsByTagName("script")[0]; g.type="text/javascript";
+  g.defer=true; g.async=true; g.src=u+"piwik.js"; s.parentNode.insertBefore(g,s);
+})();
+/** Fin Piwik **/
