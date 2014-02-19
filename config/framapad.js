00fc6ccd9d57abc85909be8f81a8192889c60abb
JosephK <josephk@framasoft.org>
2014-02-19 11:43:55 +0100

    Ajout piwik

--- config/framapad.js
+++ config/framapad.js
@@ -7,3 +7,17 @@ var f$_css_position = 'end';
 var f$_modal_don_txtdl1 = 'd\'utiliser';
 var f$_modal_don_txtdl2 = 'créer un pad';
 var f$_modal_don_liendl = 'a[href*=".framapad.org/p/"]';
+
+/** Piwik **/
+var _paq = _paq || [];
+_paq.push(["trackPageView"]);
+_paq.push(["enableLinkTracking"]);
+
+(function() {
+  var u=(("https:" == document.location.protocol) ? "https" : "http") + "://stats.framasoft.org/";
+  _paq.push(["setTrackerUrl", u+"piwik.php"]);
+  _paq.push(["setSiteId", "4"]);
+  var d=document, g=d.createElement("script"), s=d.getElementsByTagName("script")[0]; g.type="text/javascript";
+  g.defer=true; g.async=true; g.src=u+"piwik.js"; s.parentNode.insertBefore(g,s);
+})();
+/** Fin Piwik **/
