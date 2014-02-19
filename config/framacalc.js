e65858a924607ee767dda43a476fe5305b917bbc
JosephK <josephk@framasoft.org>
2014-02-19 11:39:55 +0100

    Ajout piwik

--- config/framacalc.js
+++ config/framacalc.js
@@ -23,5 +23,17 @@ if(f$_url.indexOf('framacalc.org/_start') > -1) {
   var f$_donate = false;
 }
 
+/** Piwik **/
+var _paq = _paq || [];
+_paq.push(["trackPageView"]);
+_paq.push(["enableLinkTracking"]);
 
+(function() {
+  var u=(("https:" == document.location.protocol) ? "https" : "http") + "://stats.framasoft.org/";
+  _paq.push(["setTrackerUrl", u+"piwik.php"]);
+  _paq.push(["setSiteId", "11"]);
+  var d=document, g=d.createElement("script"), s=d.getElementsByTagName("script")[0]; g.type="text/javascript";
+  g.defer=true; g.async=true; g.src=u+"piwik.js"; s.parentNode.insertBefore(g,s);
+})();
+/** Fin Piwik **/
 
