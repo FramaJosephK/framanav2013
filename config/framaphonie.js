89b391cea8f3f5c3b63ae891f6a079ced56b4ed8
JosephK <josephk@framasoft.org>
2014-02-19 11:45:30 +0100

    Ajout piwik

--- config/framaphonie.js
+++ config/framaphonie.js
@@ -8,3 +8,17 @@ var f$_jquery = 'fQuery';
 var f$_css_position = 'end';						
 
 var f$_nav_extra_css = true;
+
+/** Piwik **/
+var _paq = _paq || [];
+_paq.push(["trackPageView"]);
+_paq.push(["enableLinkTracking"]);
+
+(function() {
+  var u=(("https:" == document.location.protocol) ? "https" : "http") + "://stats.framasoft.org/";
+  _paq.push(["setTrackerUrl", u+"piwik.php"]);
+  _paq.push(["setSiteId", "14"]);
+  var d=document, g=d.createElement("script"), s=d.getElementsByTagName("script")[0]; g.type="text/javascript";
+  g.defer=true; g.async=true; g.src=u+"piwik.js"; s.parentNode.insertBefore(g,s);
+})();
+/** Fin Piwik **/
