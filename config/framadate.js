881d7c8da1797dcccfb2278aefe94c196162629d
JosephK <josephk@framasoft.org>
2014-02-19 11:41:29 +0100

    Ajout piwik

--- config/framadate.js
+++ config/framadate.js
@@ -11,3 +11,17 @@ if(f$_url.indexOf('infos_sondage.php?choix_sondage') > -1) {
   
   var f$_nav_extra_css = true;
 }
+
+/** Piwik **/
+var _paq = _paq || [];
+_paq.push(["trackPageView"]);
+_paq.push(["enableLinkTracking"]);
+
+(function() {
+  var u=(("https:" == document.location.protocol) ? "https" : "http") + "://stats.framasoft.org/";
+  _paq.push(["setTrackerUrl", u+"piwik.php"]);
+  _paq.push(["setSiteId", "10"]);
+  var d=document, g=d.createElement("script"), s=d.getElementsByTagName("script")[0]; g.type="text/javascript";
+  g.defer=true; g.async=true; g.src=u+"piwik.js"; s.parentNode.insertBefore(g,s);
+})();
+/** Fin Piwik **/
