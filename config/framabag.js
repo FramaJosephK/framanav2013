243b1e11f2c928b05e470665971c4700ff2676ad
JosephK <josephk@framasoft.org>
2014-04-24 16:09:30 +0200

    Update framabag.js

--- config/framabag.js
+++ config/framabag.js
@@ -20,7 +20,7 @@ _paq.push(["trackPageView"]);
 _paq.push(["enableLinkTracking"]);
 
 (function() {
-  var u=(("https:" == document.location.protocol) ? "https" : "http") + "://stats.framasoft.org/";
+  var u="http://stats.framabag.org/";
   _paq.push(["setTrackerUrl", u+"piwik.php"]);
   _paq.push(["setSiteId", "21"]);
   var d=document, g=d.createElement("script"), s=d.getElementsByTagName("script")[0]; g.type="text/javascript";
