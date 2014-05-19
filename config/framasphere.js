89cef3a4d7f414c98348fd533873305c924ea6ef
JosephK <josephk@framasoft.org>
2014-05-19 14:26:33 +0200

    Create framasphere.js

---
+++ config/framasphere.js
@@ -0,0 +1,15 @@
+var f$_config = 'local';
+
+/** Piwik **/
+var _paq = _paq || [];
+_paq.push(["trackPageView"]);
+_paq.push(["enableLinkTracking"]);
+
+(function() {
+  var u="http://stats.framabag.org/";
+  _paq.push(["setTrackerUrl", u+"piwik.php"]);
+  _paq.push(["setSiteId", "26"]);
+  var d=document, g=d.createElement("script"), s=d.getElementsByTagName("script")[0]; g.type="text/javascript";
+  g.defer=true; g.async=true; g.src=u+"piwik.js"; s.parentNode.insertBefore(g,s);
+})();
+/** Fin Piwik **/
