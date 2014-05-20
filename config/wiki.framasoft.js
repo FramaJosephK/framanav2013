bece2264b4f97ca66a1f94bc321e4cdc02865637
JosephK <josephk@framasoft.org>
2014-05-20 16:04:40 +0200

    Variable extra.css
    Code pour l'insertion du fichier sans attendre le dom est intégré à nav.js donc on en a plus besoin et on peut réutiliser la variable classique f$_extra_css = true;

--- config/wiki.framasoft.js
+++ config/wiki.framasoft.js
@@ -4,12 +4,7 @@ var f$_jquery = 'fQuery';
 
 var f$_alert_text = '';
 
-// Extra CSS sans attendre chargement du DOM (à intégrer dans nav.js faire de même avec nav.css)
-var f$_extra_css_link = document.createElement('link');
-    f$_extra_css_link.rel = "stylesheet";
-    f$_extra_css_link.media="screen";
-    f$_extra_css_link.href= f$_nav+"config/wiki.framasoft_extra.css";
-document.getElementById('nav_js').parentNode.insertBefore(f$_extra_css_link, document.getElementById('nav_js').nextSibling);
+var f$_nav_extra_css = true;
 
 /** Piwik **/
 var _paq = _paq || [];
