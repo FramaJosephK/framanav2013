374780695cbea4500d1e229b755ce999704d3631
JosephK <josephk@framasoft.org>
2014-05-28 16:11:01 +0200

    Update framindmap.js

--- config/framindmap.js
+++ config/framindmap.js
@@ -5,7 +5,7 @@ var f$_jquery = 'fQuery';
 var f$_url = window.location.href;
 if(f$_url.indexOf('framindmap.html') > -1) {
 // Si on n'est dans framindmap
-  var f$_nav_extra_css = true;
+  var f$_extra_css = true;
 
 // Force la désactivation des fenêtres modales, du bandeau et du macaron
   var f$_alert_text = '';
