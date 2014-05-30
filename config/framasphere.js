4b75adb51a1de6ca407307652803c89c79e067dd
Luc Didry <ldidry@users.noreply.github.com>
2014-05-30 15:12:19 +0200

    Update framasphere.js
    Plus besoin de piwik dans la nav car piwik est en dur dans framasphere

--- config/framasphere.js
+++ config/framasphere.js
@@ -12,17 +12,3 @@ var f$_bootstrap_js = 'html';
 jQuery(document).ready(function() {
     jQuery('link[href*=bootstrap-complete]').remove();
 });
-
-/** Piwik **/
-var _paq = _paq || [];
-_paq.push(["trackPageView"]);
-_paq.push(["enableLinkTracking"]);
-
-(function() {
-  var u="https://framasphere.org/piwik/";
-  _paq.push(["setTrackerUrl", u+"piwik.php"]);
-  _paq.push(["setSiteId", "26"]);
-  var d=document, g=d.createElement("script"), s=d.getElementsByTagName("script")[0]; g.type="text/javascript";
-  g.defer=true; g.async=true; g.src=u+"piwik.js"; s.parentNode.insertBefore(g,s);
-})();
-/** Fin Piwik **/
