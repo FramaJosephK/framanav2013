d92a5aa561387bc63fe5200e70fabf300eab5d85
JosephK <josephk@framasoft.org>
2014-06-12 10:27:16 +0200

    Ajout bandeau campagne MyPads

--- config/framapad.js
+++ config/framapad.js
@@ -4,9 +4,17 @@ var f$_jquery = 'fQuery';
 
 var f$_css_position = 'end';	
 
+/*
 var f$_modal_don_txtdl1 = 'd\'utiliser';
 var f$_modal_don_txtdl2 = 'créer un pad';
 var f$_modal_don_liendl = 'a[href*=".framapad.org/p/"]';
+*/
+
+var f$_alert_type = 'warning'; // warning = jaune ; danger = rouge ; info = bleu ; success = vert
+var f$_alert_cookie = 24*60*60*1000;	// durée d'expiration du cookie en milliseconde
+var f$_alert_text = '<p style="max-width:900px; font-weight:bold; margin:0 auto; text-align:center">Soutenez <a href="http://www.ulule.com/etherpad-framapad">la campagne de financement participatif « MyPads »</a>, lancée par Framasoft,<br />'+
+                    ' pour la création d’un formidable plugin pour Etherpad/Framapad !<p>';	
+
 
 /** Piwik **/
 var _paq = _paq || [];
