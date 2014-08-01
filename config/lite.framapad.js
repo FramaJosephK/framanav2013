e016eb87c016f95cb32229b2021fdf7503a9de3d
JosephK <josephk@framasoft.org>
2014-08-01 10:55:08 +0200

    Update lite.framapad.js

--- config/lite.framapad.js
+++ config/lite.framapad.js
@@ -9,15 +9,6 @@ var f$_alert_text = '';
 var f$_modal_don_liendl = '';
 var f$_donate = false;
 
-
-// Message arrêt de la création des pads
-var f$_alert_modal_title = 'Création des pads désactivée';
-var f$_alert_modal_text = '<p>Nous vous informons que cette instance de Framapad (<b>lite</b>.framapad.org) ne peut plus accueillir de nouveaux pads.<p>'+
-    '<br /><p>Il reste bien évidement possible de travailler sur les pads déjà existants (ils ne seront pas supprimés)'+
-    ' mais pour en créer de nouveaux, veuillez passer par la page d\'accueil du site :<br />'+
-    '<a href="http://www.framapad.org">www.framapad.org</a></p>'+
-    '<br /><p>Merci.<br />L\'équipe technique</p>';
-
 /** Piwik **/
 var _paq = _paq || [];
 _paq.push(["trackPageView"]);
